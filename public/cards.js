667d4b811ce0535a71fb58d38b3386ac5f66c02c
Ayush Yadav <ayush421301@gmail.com>
2024-06-01 20:46:29 +0530

    Added Js Functionality

--- public/cards.js
+++ public/cards.js
@@ -1,29 +1,10 @@
-function showDesignCards() {
-  var designCards = document.getElementById("design-cards");
-  var marketingCards = document.getElementById("marketing-cards");
-  var financeCards = document.getElementById("finance-cards");
-  var musicCards = document.getElementById("music-cards");
-  var educationCards = document.getElementById("education-cards");
-  if (designCards.style.display === "none") {
-    designCards.style.display = "block";
-    marketingCards.style.display = "none";
-    financeCards.style.display = "none";
-    musicCards.style.display = "none";
-    educationCards.style.display = "none";
-  }
-}
-
-function showMarketingCards() {
-  var designCards = document.getElementById("design-cards");
-  var marketingCards = document.getElementById("marketing-cards");
-  var financeCards = document.getElementById("finance-cards");
-  var musicCards = document.getElementById("music-cards");
-  var educationCards = document.getElementById("education-cards");
-  if (
-    marketingCards.style.display === "none" ||
-    (marketingCards.style.display === "" &&
-      designCards.style.display === "block")
-  )
-    marketingCards.style.display = "block";
-  else marketingCards.style.display = "none";
+function showCards(cardId) {
+  var cardSections = document.querySelectorAll(".cards");
+  cardSections.forEach(function (section) {
+    if (section.id === cardId) {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  });
 }
